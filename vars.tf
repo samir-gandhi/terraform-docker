eb4ff1c479c043906d9fc8c6f8c2b9122596b77c
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2023-07-11 14:07:00 -0600

    try with ping example

---
+++ vars.tf
@@ -0,0 +1,34 @@
+variable "pingone_username" {
+  type = string
+}
+
+variable "pingone_password" {
+  type = string
+}
+
+variable "pingone_environment_id" {
+  type = string
+}
+
+variable "pingone_region" {
+  type = string
+}
+
+variable "pingone_license_id" {
+  type = string
+}
+
+variable "pingone_client_id" {
+  type = string
+}
+
+variable "pingone_client_secret" {
+  type = string
+}
+
+variable "pingone_username" {
+}
+variable "pingone_environment_id" {
+}
+variable "pingone_license_id" {
+}
